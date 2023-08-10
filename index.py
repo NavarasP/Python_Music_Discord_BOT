5b14e24bba441ff72160d104944c4e4ca5bf02cd
Navaras P <69687392+NavarasP@users.noreply.github.com>
2023-08-10 12:46:21 +0530

    Solved

--- index.py
+++ index.py
@@ -11,14 +11,14 @@ import constants
 
 intents = nextcord.Intents.all()
 song_queue = Queue()
-bot = commands.Bot(command_prefix="@", intents=intents)
+bot = commands.Bot(command_prefix="Alexa ", intents=intents)
 
 is_playing = False
 music_bot_channel_id=""
 intents.voice_states = True
 intents.members = True
 intents.messages = True
-import youtube_dl
+
 
 
 
@@ -30,6 +30,7 @@ def download_audio(video_url):
     audio_stream = youtube.streams.filter(only_audio=True).first()
     audio_file_path = audio_stream.download()
     return audio_file_path
+    
 def delete_audio_file(file_path):
     try:
         os.remove(file_path)
@@ -62,14 +63,8 @@ async def join(ctx):
     await ctx.send(f"I have joined the voice channel {voice_channel}.")
 
 
-@bot.command(name='add')
-async def add(ctx, *args):
-
-    if not args:
-        await ctx.send("Please provide a song name, search query, or playlist URL.")
-        return
-    search_query = " ".join(args)
-
+async def add_to_queue(ctx,search_query):
+    search_query=search_query
     if "youtube.com/playlist" in search_query:
         playlist_url = search_query
         playlist = Playlist(playlist_url)
@@ -98,8 +93,36 @@ async def add(ctx, *args):
             await ctx.send(f"Added {results[0]['title']} to the queue.")
 
 
+
+@bot.command(name='add')
+async def add(ctx, *args):
+
+    if not args:
+        await ctx.send("Please provide a song name, search query, or playlist URL.")
+        return
+    search_query = " ".join(args)
+    add_to_queue(ctx,search_query)
+
+
+
+async def play_songfrom_queue(ctx):
+    global is_playing
+    while not song_queue.empty():
+            song = song_queue.get()
+            await ctx.send(f"Now playing: {song}")
+            is_playing = True
+            audio_file_path = download_audio(song)
+            ctx.voice_client.play(nextcord.FFmpegOpusAudio(audio_file_path), after=lambda e: print(f'Player error: {e}') if e else None)
+            while ctx.voice_client.is_playing():
+                await asyncio.sleep(1)
+
+            delete_audio_file(audio_file_path)
+    is_playing = False
+
+
+
 @bot.command(name='play', pass_context=True)
-async def play(ctx):
+async def play(ctx, *, args=""):
     global is_playing
     if ctx.voice_client is None:
         if ctx.author.voice is None:
@@ -107,23 +130,34 @@ async def play(ctx):
             return
         voice_channel = ctx.author.voice.channel
         await voice_channel.connect()
-    if song_queue.empty():
+
+    if not args:
+        if song_queue.empty():
+            await ctx.send("The queue is empty. Use the `add` command to add songs to the queue.")
+            is_playing = False
+            return
+        else:
+            play_songfrom_queue(ctx)
+            
+    else:
+        search_query = " ".join(args)
+        add_to_queue(ctx,search_query)
+        if not is_playing:
+            play_songfrom_queue(ctx)
+        
+        
+@bot.command(name='queue')
+async def show_queue(ctx):
+    if not song_queue:
         await ctx.send("The queue is empty. Use the `add` command to add songs to the queue.")
-        is_playing = False
         return
 
-    while not song_queue.empty():
-        song = song_queue.get()
-        await ctx.send(f"Now playing: {song}")
-        is_playing = True
-        audio_file_path = download_audio(song)
-        ctx.voice_client.play(nextcord.FFmpegOpusAudio(audio_file_path ), after=lambda e: print(f'Player error: {e}') if e else None)
-        while ctx.voice_client.is_playing():
-            await asyncio.sleep(1)
+    queue_text = "Current song queue:\n"
+    for idx, song in enumerate(song_queue, start=1):
+        queue_text += f"{idx}. {song}\n"
 
-        delete_audio_file(audio_file_path)
+    await ctx.send(queue_text)
 
-    is_playing=False
 
 @bot.command(name='pause', pass_context=True)
 async def pause(ctx):
@@ -148,6 +182,24 @@ async def skip(ctx):
         await ctx.send("Skipped the current song.")
     else:
         await ctx.send("No song is currently playing.")
+
+@bot.command(name='skipto')
+async def skipto(ctx, index: int):
+    global is_playing
+    if index < 1 or index > len(song_queue):
+        await ctx.send("Invalid song index.")
+        return
+
+    if ctx.voice_client and ctx.voice_client.is_playing():
+        ctx.voice_client.stop()
+
+    is_playing = False
+    song_to_play = song_queue[index - 1]
+    song_queue.remove(song_to_play)
+
+    await play_songfrom_queue(ctx)
+    
+
 
 @bot.command(name='stop')
 async def stop(ctx):
